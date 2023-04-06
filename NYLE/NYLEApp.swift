f04e52010ddca9c545e3cde8c6629878c2b5f5ac
Rema Fawaz <Remaalnssiry@gmail.com>
2023-04-06 04:30:39 +0300

    Initial Commit

---
+++ NYLE/NYLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYLEApp.swift
+//  NYLE
+//
+//  Created by Rema Alnssiry on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct NYLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
