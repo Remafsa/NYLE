09e4e435dc249ea7e3435135fabddf41bb4041d9
Rema Fawaz <Remaalnssiry@gmail.com>
2023-04-07 04:33:19 +0300

    Adding Fonts File

---
+++ NYLE/View/FarmerSignUpView.swift
@@ -0,0 +1,26 @@
+//
+//  FarmerSignUpView.swift
+//  NYLE
+//
+//  Created by Rema Alnssiry on 06/04/2023.
+//
+
+import SwiftUI
+
+struct FarmerSignUpView: View {
+    var body: some View {
+        HStack {
+            Text("سجل دخولك إلى حساب المزارع")
+                
+            
+            
+        }
+      
+    }
+}
+
+struct FarmerSignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        FarmerSignUpView()
+    }
+}
